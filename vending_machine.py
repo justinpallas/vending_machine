a0c5d515450fcf8ea4a1b495177ebb1b15a546c9
Justin Pallas <justin.pallas@uni-ulm.de>
2023-05-01 17:23:41 +0200

    add comments

--- vending_machine.py
+++ vending_machine.py
@@ -1,5 +1,6 @@
 class VendingMachine:
     def __init__(self):
+        # initializing self.items and adding sample items
         self.items = {
             "snacks": [
                 {"name": "chips", "price": 1.50, "weight": 50, "quantity": 10},
@@ -16,8 +17,10 @@ class VendingMachine:
     def display_items(self):
         """display all available items"""
         print("\n\tAvailable items:")
+        # iterating over all categories and items in self.items and printing each item
         for category, items in self.items.items():
             print(f"\n\t{category.capitalize()}:")
+            # putting the unit grams (g) after every weight and the unit milliliters (ml) after every volume
             attribute_suffix = "g" if category == "snacks" else "ml"
             for item in items:
                 print(
@@ -26,12 +29,14 @@ class VendingMachine:
 
     def add_item(self, category, name, price, attribute, attribute_value):
         """add an item to the vending machine"""
+        # append the given item to the corresponding category list with a default quantity of 10
         self.items[category].append({"name": name,"price": price, f"{attribute}": attribute_value, "quantity": 10})
         print(f"\n\t{name.capitalize()} added to {category}!")
 
 
     def remove_item(self, category, name):
         """remove an item from the vending machine"""
+        # searching the category's list for the given item name and remove it if found
         for i, item in enumerate(self.items[category]):
             if item["name"] == name:
                 del self.items[category][i]
@@ -42,6 +47,7 @@ class VendingMachine:
 
     def restock_item(self, category, name):
         """restock a certain item in the vending machine"""
+        # seaching the category's list for the given item name and if found, setting it's quantity to 10
         for item in self.items[category]:
             if item["name"] == name:
                 item["quantity"] = 10
@@ -52,19 +58,21 @@ class VendingMachine:
 
     def purchase_item(self, category, name):
         """purchase an item from the vending machine"""
+        # searching the category's list for the given item name and if found, decreasing its quantity by 1
         for item in self.items[category]:
             if item["name"] == name:
                 if item["quantity"] > 0:
                     item["quantity"] -= 1
-                    print(f"{name.capitalize()} purchased!")
+                    print(f"\n\t{name.capitalize()} purchased!")
                     break
                 else:
-                    print(f"{name.capitalize()} is out of stock.")
+                    print(f"\n\t{name.capitalize()} is out of stock.")
                     break
         else:
             print(f"{name.capitalize()} not found in {category}.")
         
     def is_full(self):
+        """check if the vending machine is full (returns bool)"""
         total_items = len(self.items["snacks"]) + len(self.items["beverages"])
         if total_items < 10:
             return False
@@ -72,12 +80,20 @@ class VendingMachine:
             return True
         
     def is_empty(self):
+        """check if the vending machine is empty (returns bool)"""
         total_items = len(self.items["snacks"]) + len(self.items["beverages"])
         if total_items > 0:
             return False
         else:
             return True
 
+def get_category():
+    """prompt the user for a category and check if it is valid"""
+    category = input("Enter category (snacks/beverages): ") 
+    while category != "snacks" and category != "beverages":
+        print("\n\tInvalid category. Please try again.")
+        category = input("\nEnter category (snacks/beverages): ")
+    return category
 
 if __name__ == "__main__":
     vending_machine = VendingMachine()
@@ -97,18 +113,17 @@ if __name__ == "__main__":
 
         choice = input("\nEnter choice: ")
 
+        # choice 1: Display available items
         if choice == "1":
             if vending_machine.is_empty() == False:
                 vending_machine.display_items()
             else:
                 print("\n\tThe vending machine is currently empty.")
 
+        # choice 2: Add an item
         elif choice == "2":
             if vending_machine.is_full() == False:
-                category = input("Enter category (snacks/beverages): ").lower()
-                while category != "snacks" and category != "beverages":
-                    print("\n\tInvalid category. Please try again.")
-                    category = input("\nEnter category (snacks/beverages): ")
+                category = get_category()
                 attribute = "weight" if category == "snacks" else "volume"
                 name = input("Enter name: ")
                 while True:
@@ -127,30 +142,34 @@ if __name__ == "__main__":
             else:
                 print("\n\tThe vending machine is already full. Please remove an item before adding another one!")
 
+        # choice 3: Remove an item
         elif choice == "3":
             if vending_machine.is_empty() == False:
-                category = input("Enter category (snacks/beverages): ")
+                category = get_category()
                 name = input("Enter name: ")
                 vending_machine.remove_item(category, name)
             else:
                 print("\n\tThe vending machine is already empty. Please add an item first!")
 
+        # choice 4: Restock an item
         elif choice == "4":
             if vending_machine.is_empty() == False:
-                category = input("Enter category (snacks/beverages): ")
+                category = get_category()
                 name = input("Enter name: ")
                 vending_machine.restock_item(category, name)
             else:
                 print("\n\tThere are no items in the vending machine. Please add an item first!")
 
+        # choice 5: Purchase an item
         elif choice == "5":
             if vending_machine.is_empty() == False:
-                category = input("Enter category (snacks/beverages): ")
+                category = get_category()
                 name = input("Enter name: ")
                 vending_machine.purchase_item(category, name)
             else: 
                 print("\n\tThere are no items in the vending machine. Please add an item first!")
 
+        # choice 6: Exit the program
         elif choice == "6":
             print("\n\tExiting program.")
             break
